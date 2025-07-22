ab5d1cb494802e7baccc791df130caacd8d6cec4
xebec19 <rohandeveloper106@gmail.com>
2025-07-22 08:48:14 +0530

    wip campaign factory

--- contracts/Campaign.sol
+++ contracts/Campaign.sol
@@ -1,67 +1,7 @@
 pragma solidity ^0.8.28;
 
-contract Campaign {
-    address public chairperson;
-    uint public minAmount;
+contract CampaignFactory {
+    event CampaignCreated(address manager, uint val);
 
-    struct Contribution {
-        string name;
-        uint value;
-        bool exists;
-    }
-
-    mapping(address => Contribution) public contributions;
-
-    struct Request {
-        string title;
-        uint votes;
-        address payable recipient;
-        string description;
-        bool completed;
-        uint approvalCount;
-        mapping(address => bool) approvals;
-        address[] voters;
-    }
-
-    Request[] public requests;
-    uint public requestCount;
-
-    constructor(uint amt) {
-        chairperson = msg.sender;
-        minAmount = amt;
-    }
-
-    function contribute(string calldata name) external payable {
-        require(msg.sender != chairperson, "Chairperson can not contribute");
-        require(msg.value < minAmount, "Value is too low!");
-        require(
-            !contributions[msg.sender].exists,
-            "User has already contributed"
-        );
-
-        contributions[msg.sender] = Contribution(name, msg.value, true);
-    }
-
-    function createRequest(
-        string calldata title,
-        string calldata description,
-        address payable recipient
-    ) public {
-        requestCount++;
-        Request storage req = requests[requestCount];
-        req.title = title;
-        req.description = description;
-        req.recipient = recipient;
-        req.approvalCount = 0;
-    }
-
-    function approveRequest(uint index) public {
-        require(msg.sender != chairperson, "Chairperson can not approve");
-        require(contributions[msg.sender].exists, "You must contribute first");
-        require(index < requestCount, "Request does not exist");
-        require(requests[index].vo)
-        Request storage req = requests[index];
-
-
-    }
+    function createCampaign() external payable {}
 }
